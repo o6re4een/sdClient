260bdadffed6c2f6d2755140ad7ed4d65f85cf63
o6re4een <artem.portnov2014@gmail.com>
2022-09-09 17:08:41 +0500

    api fix2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
+var https = require('https');
+var http = require('http');
 const path = require("path")
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 80
 
 const app = express()
 app.use(express.static(__dirname))
@@ -9,4 +11,4 @@ app.use(express.static(path.resolve(__dirname, 'build')))
 app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-app.listen(PORT)
+http.createServer(app).listen(80);
